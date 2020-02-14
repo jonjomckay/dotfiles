18861dd9033549dcb371d540c8009f4df8a95e7b
Jonjo McKay <jonjo@jonjomckay.com>
2020-02-14 14:20:31 +0000

    Added screen layout and keyboard configuration

---
+++ i3/.screenlayout/office.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DVI-I-1 --off --output DVI-D-1 --off --output HDMI-1 --off --output DP-1 --off --output DP-1-8 --primary --mode 2560x1440 --pos 0x481 --rotate normal --output DP-1-1 --mode 2560x1440 --pos 2560x0 --rotate right
