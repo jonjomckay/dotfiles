8a0d5bd26af718eab39097c6d1ed16d0649504ef
Jonjo McKay <jonjo@jonjomckay.com>
2020-08-16 12:30:09 +0100

    Split things into Darwin and Linux

--- nix/.config/nixpkgs/home.nix
+++ nix/.config/nixpkgs/home.nix
@@ -1,7 +1,14 @@
 { config, lib, pkgs, ... }:
 
+# Thanks to https://www.reddit.com/r/NixOS/comments/ae9q01/how_to_os_from_inside_a_nix_file/ednqzap/
+let
+  machine = import ./machine.nix;
+  isArch = machine.operatingSystem == "Arch Linux";
+  isDarwin = machine.operatingSystem == "Darwin";
+in
 {
-  imports = [ ./waybar.nix ];
+  imports = [ ./waybar.nix ]
+    ++ (if isArch then [ ./linux.nix ] else [ ]);
 
   home.keyboard.layout = "gb";
   home.packages = [
@@ -10,43 +17,45 @@
     pkgs.aws-iam-authenticator
     pkgs.bat
     pkgs.bind
-    pkgs.blueman
-    pkgs.bmon
     pkgs.dejavu_fonts
     pkgs.docker-compose
     pkgs.fira-code
     pkgs.font-awesome
-    pkgs.gnome3.gnome-calculator
-    pkgs.gnome3.gnome-screenshot
     pkgs.htop
-    pkgs.iotop
     pkgs.ipafont
     pkgs.jq
-    pkgs.keepassxc
     pkgs.kind
     pkgs.maven
-    pkgs.meld
-    pkgs.networkmanager-openvpn
+    pkgs.nixpkgs-fmt
     pkgs.ngrok
     pkgs.nodejs
+    pkgs.rsync
+    pkgs.whois
+    pkgs.youtube-dl
+  ]
+  ++ lib.optional isArch [
+    pkgs.blueman
+    pkgs.bmon
+    pkgs.gnome3.gnome-calculator
+    pkgs.gnome3.gnome-screenshot
+    pkgs.iotop
+    pkgs.keepassxc
+    pkgs.meld
+    pkgs.networkmanager-openvpn
     pkgs.pulsemixer
     pkgs.python38
     pkgs.python38Packages.i3ipc
-    pkgs.rsync
     pkgs.slack
     pkgs.sublime3-dev
     pkgs.vscodium
-    pkgs.whois
-    pkgs.youtube-dl
-    pkgs.zoom-us
   ];
+  # ++ lib.optional isDarwin [ ];
 
   # These are required for non-NixOS installations
   home.sessionVariables._JAVA_AWT_WM_NONREPARENTING = "1";
   home.sessionVariables.EDITOR = "vim";
   home.sessionVariables.FONTCONFIG_FILE = "${pkgs.fontconfig.out}/etc/fonts/fonts.conf";
-  home.sessionVariables.LD_LIBRARY_PATH = "$(nixGL printenv LD_LIBRARY_PATH):$LD_LIBRARY_PATH";
-  home.sessionVariables.LOCALE_ARCHIVE = "${pkgs.glibcLocales}/lib/locale/locale-archive";
+  
   home.sessionVariables.XDG_DATA_DIRS = "$HOME/.nix-profile/share:$HOME/.share:$XDG_DATA_DIRS:-/usr/local/share/:/usr/share";
 
   fonts.fontconfig.enable = true;
@@ -113,13 +122,6 @@
     };
   };
 
-  programs.feh.enable = true;
-
-  programs.firefox = {
-    enable = true;
-    package = pkgs.firefox-devedition-bin;
-  };
-
   programs.git.enable = true;
   programs.git.userEmail = "jonjo@jonjomckay.com";
   programs.git.userName = "Jonjo McKay";
@@ -128,19 +130,40 @@
 
   programs.gpg.enable = true;
 
-  programs.keychain.enable = true;
-  programs.keychain.enableXsessionIntegration = true;
-
   programs.kitty.enable = true;
   programs.kitty.font.name = "Fira Code Retina";
   programs.kitty.extraConfig = builtins.readFile ./configs/kitty.conf;
 
-  programs.rofi.enable = true;
-  programs.rofi.font = "Fira Code Retina 10";
-  programs.rofi.theme = "solarized";
-
   programs.ssh.enable = true;
   programs.ssh.compression = true;
+  programs.ssh.matchBlocks = {
+    "jack" = {
+      hostname = "jonjomckay.nsupdate.info";
+      port = 2200;
+      localForwards = [
+        {
+          bind.port = 6789;
+          host.address = "localhost";
+          host.port = 6789;
+        }
+        {
+          bind.port = 7878;
+          host.address = "localhost";
+          host.port = 7878;
+        }
+        {
+          bind.port = 8989;
+          host.address = "localhost";
+          host.port = 8989;
+        }
+        {
+          bind.port = 9091;
+          host.address = "localhost";
+          host.port = 9091;
+        }
+      ];
+    };
+  };
 
   programs.starship.enable = true;
   programs.starship.enableZshIntegration = true;
@@ -151,136 +174,16 @@
   programs.zsh.enableAutosuggestions = true;
   programs.zsh.enableCompletion = true;
   programs.zsh.oh-my-zsh.enable = true;
-
-  services.blueman-applet.enable = true;
-
-  services.dunst.enable = true;
-  services.dunst.settings = {
-    global = {
-      alignment = "center";
-      follow = "mouse";
-      format = "<b>%s</b>\n%b";
-      geometry = "300x50-15+49";
-      frame_width = 3;
-      frame_color = "#8EC07C";
-      horizontal_padding = 6;
-      line_height = 3;
-      padding = 6;
-      separator_color = "frame";
-      separator_height = 2;
-      transparency = 5;
-      word_wrap = "yes";
-    };
-
-    urgency_low = {
-      frame_color = "#3B7C87";
-      foreground = "#3B7C87";
-      background = "#191311";
-      timeout = 4;
-    };
-
-    urgency_normal = {
-      frame_color = "#5B8234";
-      foreground = "#5B8234";
-      background = "#191311";
-      timeout = 6;
-    };
-
-    urgency_critical = {
-     frame_color = "#B7472A";
-      foreground = "#B7472A";
-      background = "#191311";
-      timeout = 8;
-    };
-  };
-
-  services.gnome-keyring.enable = true;
-
-  services.gpg-agent = {
-    enable = true;
-  };
-
-  services.network-manager-applet = {
-    enable = true;
-  };
-  
-  services.syncthing.enable = true;
-
-  services.waybar.enable = true;
-  services.waybar.package = pkgs.waybar.override {
-    pulseSupport = true;
-  };
-  services.waybar.config = builtins.readFile ./configs/waybar.json;
-  services.waybar.styles = builtins.readFile ./configs/waybar.css;
-
-  xresources.extraConfig = builtins.readFile ./configs/xresources.conf;
-
-  wayland.windowManager.sway.enable = true;
-  wayland.windowManager.sway.config.bars = [];
-  wayland.windowManager.sway.config.colors.focused = {
-     border = "#007030";
-     background = "#007030";
-     childBorder = "#285577";
-     text = "#ffffff";
-     indicator = "2b2b2b";
-  };
-  wayland.windowManager.sway.config.colors.focusedInactive = {
-     border = "#888888";
-     background = "#2b2b2b";
-     childBorder = "#5f676a";
-     text = "#ffffff";
-     indicator = "2b2b2b";
-  };
-  wayland.windowManager.sway.config.colors.unfocused = {
-     border = "#888888";
-     background = "#2b2b2b";
-     childBorder = "#222222";
-     text = "#ffffff";
-     indicator = "2b2b2b";
-  };
-  wayland.windowManager.sway.config.colors.urgent = {
-     border = "#900000";
-     background = "#900000";
-     childBorder = "#900000";
-     text = "#ffffff";
-     indicator = "2b2b2b";
-  };
-  wayland.windowManager.sway.config.gaps.inner = 15;
-  wayland.windowManager.sway.config.gaps.top = 0;
-  wayland.windowManager.sway.config.input = {
-    "*" = {
-      xkb_layout = "gb";
-    };
-
-    "1452:635:bcm5974" = {
-      accel_profile = "adaptive";
-      click_method = "button_areas";
-      natural_scroll = "enabled";
-      pointer_accel = "0.55";
-    };
-  };
-  wayland.windowManager.sway.config.modifier = "Mod4";
-  wayland.windowManager.sway.config.output = {
-    "*" = {
-      bg = "${./anime-minimalism-4k-4e.jpg} fill";
-    };
-  };
-  wayland.windowManager.sway.config.startup = [
+  programs.zsh.initExtra = ''
+    . "${pkgs.nix}/etc/profile.d/nix.sh"
+  '';
+  programs.zsh.zplug.enable = true;
+  programs.zsh.zplug.plugins = [
     {
-      command = "swaymsg workspace 1";
-    }
-    {
-      command = "${(pkgs.python38.withPackages (ps: [ps.i3ipc])).interpreter} ${./fader.py}";
+      name = "zsh-users/zsh-syntax-highlighting";
+      tags = [ "defer:2" ];
     }
   ];
-  wayland.windowManager.sway.config.terminal = "nixGL alacritty";
-  wayland.windowManager.sway.config.window.border = 0;
-
-  wayland.windowManager.sway.config.keybindings = let modifier = config.wayland.windowManager.sway.config.modifier;
-  in lib.mkOptionDefault {
-    "${modifier}+0" = "workspace number 1";
-    "${modifier}+space" = "exec rofi -show drun";
-  };
 
   # This value determines the Home Manager release that your
   # configuration is compatible with. This helps avoid breakage
---
+++ nix/.config/nixpkgs/linux.nix
@@ -0,0 +1,150 @@
+{ config, lib, pkgs, ... }:
+
+{
+  home.sessionVariables.LD_LIBRARY_PATH = "$(nixGL printenv LD_LIBRARY_PATH):$LD_LIBRARY_PATH";
+  home.sessionVariables.LOCALE_ARCHIVE = "${pkgs.glibcLocales}/lib/locale/locale-archive";
+
+  programs.feh.enable = true;
+
+  programs.firefox = {
+    enable = true;
+    package = pkgs.firefox-devedition-bin;
+  };
+
+  programs.keychain.enable = true;
+  programs.keychain.enableXsessionIntegration = true;
+
+  programs.rofi.enable = true;
+  programs.rofi.font = "Fira Code Retina 10";
+  programs.rofi.theme = "solarized";
+
+  services.blueman-applet.enable = true;
+
+  services.dunst.enable = true;
+  services.dunst.settings = {
+    global = {
+      alignment = "center";
+      follow = "mouse";
+      format = "<b>%s</b>\n%b";
+      geometry = "300x50-15+49";
+      frame_width = 3;
+      frame_color = "#8EC07C";
+      horizontal_padding = 6;
+      line_height = 3;
+      padding = 6;
+      separator_color = "frame";
+      separator_height = 2;
+      transparency = 5;
+      word_wrap = "yes";
+    };
+
+    urgency_low = {
+      frame_color = "#3B7C87";
+      foreground = "#3B7C87";
+      background = "#191311";
+      timeout = 4;
+    };
+
+    urgency_normal = {
+      frame_color = "#5B8234";
+      foreground = "#5B8234";
+      background = "#191311";
+      timeout = 6;
+    };
+
+    urgency_critical = {
+     frame_color = "#B7472A";
+      foreground = "#B7472A";
+      background = "#191311";
+      timeout = 8;
+    };
+  };
+
+  services.gnome-keyring.enable = true;
+
+  services.gpg-agent = {
+    enable = true;
+  };
+
+  services.network-manager-applet = {
+    enable = true;
+  };
+  
+  services.syncthing.enable = true;
+
+  services.waybar.enable = true;
+  services.waybar.package = pkgs.waybar.override {
+    pulseSupport = true;
+  };
+  services.waybar.config = builtins.readFile ./configs/waybar.json;
+  services.waybar.styles = builtins.readFile ./configs/waybar.css;
+
+  wayland.windowManager.sway.enable = true;
+  wayland.windowManager.sway.config.bars = [];
+  wayland.windowManager.sway.config.colors.focused = {
+     border = "#007030";
+     background = "#007030";
+     childBorder = "#285577";
+     text = "#ffffff";
+     indicator = "2b2b2b";
+  };
+  wayland.windowManager.sway.config.colors.focusedInactive = {
+     border = "#888888";
+     background = "#2b2b2b";
+     childBorder = "#5f676a";
+     text = "#ffffff";
+     indicator = "2b2b2b";
+  };
+  wayland.windowManager.sway.config.colors.unfocused = {
+     border = "#888888";
+     background = "#2b2b2b";
+     childBorder = "#222222";
+     text = "#ffffff";
+     indicator = "2b2b2b";
+  };
+  wayland.windowManager.sway.config.colors.urgent = {
+     border = "#900000";
+     background = "#900000";
+     childBorder = "#900000";
+     text = "#ffffff";
+     indicator = "2b2b2b";
+  };
+  wayland.windowManager.sway.config.gaps.inner = 15;
+  wayland.windowManager.sway.config.gaps.top = 0;
+  wayland.windowManager.sway.config.input = {
+    "*" = {
+      xkb_layout = "gb";
+    };
+
+    "1452:635:bcm5974" = {
+      accel_profile = "adaptive";
+      click_method = "button_areas";
+      natural_scroll = "enabled";
+      pointer_accel = "0.55";
+    };
+  };
+  wayland.windowManager.sway.config.modifier = "Mod4";
+  wayland.windowManager.sway.config.output = {
+    "*" = {
+      bg = "${./anime-minimalism-4k-4e.jpg} fill";
+    };
+  };
+  wayland.windowManager.sway.config.startup = [
+    {
+      command = "swaymsg workspace 1";
+    }
+    {
+      command = "${(pkgs.python38.withPackages (ps: [ps.i3ipc])).interpreter} ${./fader.py}";
+    }
+  ];
+  wayland.windowManager.sway.config.terminal = "nixGL alacritty";
+  wayland.windowManager.sway.config.window.border = 0;
+
+  wayland.windowManager.sway.config.keybindings = let modifier = config.wayland.windowManager.sway.config.modifier;
+  in lib.mkOptionDefault {
+    "${modifier}+0" = "workspace number 1";
+    "${modifier}+space" = "exec rofi -show drun";
+  };
+
+  xresources.extraConfig = builtins.readFile ./configs/xresources.conf;
+};
