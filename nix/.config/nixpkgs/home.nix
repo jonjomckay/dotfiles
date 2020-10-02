6a6853adead784f98a11e96e634f7b6bbd4079b4
Jonjo McKay <jonjo@jonjomckay.com>
2020-10-02 21:07:10 +0100

    Removed a bunch of nix stuff

--- nix/.config/nixpkgs/home.nix
+++ nix/.config/nixpkgs/home.nix
@@ -21,169 +21,22 @@ in
   home.keyboard.layout = "gb";
   home.packages = [
     doom-emacs
-    pkgs.aria2
     pkgs.awscli
     pkgs.aws-iam-authenticator
-    pkgs.bat
-    pkgs.bind
-    pkgs.dejavu_fonts
     pkgs.docker-compose
-    pkgs.fira-code
-    pkgs.font-awesome
-    pkgs.htop
-    pkgs.ipafont
-    pkgs.jq
     pkgs.kind
     pkgs.kubectl
-    pkgs.maven
     pkgs.nixpkgs-fmt
     pkgs.ngrok
-    pkgs.nodejs
-    pkgs.rsync
-    pkgs.whois
     pkgs.youtube-dl
   ];
-
-  # These are required for non-NixOS installations
-  home.sessionVariables._JAVA_AWT_WM_NONREPARENTING = "1";
-  home.sessionVariables.EDITOR = "vim";
-  home.sessionVariables.FONTCONFIG_FILE = "${pkgs.fontconfig.out}/etc/fonts/fonts.conf";
-  
-  home.sessionVariables.XDG_DATA_DIRS = "$HOME/.nix-profile/share:$HOME/.share:$XDG_DATA_DIRS:-/usr/local/share/:/usr/share";
-
-  fonts.fontconfig.enable = true;
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 
-  programs.alacritty.enable = true;
-  programs.alacritty.settings = {
-    colors = {
-      primary = {
-        background = "#1d1f21";
-        foreground = "#c5c8c6";
-      };
-
-      cursor = {
-        cursor = "#c5c8c6";
-      };
-
-      normal = {
-        black = "#1d1f21";
-        red = "#f393d4";
-        green = "#82f8b8";
-        yellow = "#fff9b2";
-        blue = "#3592f4";
-        magenta = "#ea60f3";
-        cyan = "#67d5fa";
-        white = "#c5c8c6";
-      };
-
-      bright = {
-        black = "#969896";
-        red = "#ee7bca";
-        green = "#74faa9";
-        yellow = "#fffaa3";
-        blue = "#2b7bf1";
-        magenta = "#e23df0";
-        cyan = "#58cbf9";
-        white = "#ffffff";
-      };
-    };
-
-    env = {
-      TERM = "xterm-256color";
-    };
-
-    font = {
-      size = 9;
-
-      normal = {
-        family = "Fira Code Retina";
-      };
-    };
-
-    scrolling = {
-      history = 100000;
-    };
-
-    window = {
-      padding = {
-        x = 10;
-        y = 10;
-      };
-    };
-  };
-
   home.file.".emacs.d/init.el".text = ''
       (load "default.el")
   '';
-
-  programs.git.enable = true;
-  programs.git.userEmail = "jonjo@jonjomckay.com";
-  programs.git.userName = "Jonjo McKay";
-  programs.git.signing.key = "C4CC649D7F58611D";
-  programs.git.signing.signByDefault = true;
-
-  programs.gpg.enable = true;
-
-  programs.kitty.enable = true;
-  programs.kitty.font.name = "Fira Code Retina";
-  programs.kitty.extraConfig = builtins.readFile ./configs/kitty.conf;
-
-  programs.ssh.enable = true;
-  programs.ssh.compression = true;
-  programs.ssh.matchBlocks = {
-    "jack" = {
-      hostname = "jonjomckay.nsupdate.info";
-      port = 2200;
-      localForwards = [
-        {
-          bind.port = 6789;
-          host.address = "localhost";
-          host.port = 6789;
-        }
-        {
-          bind.port = 7878;
-          host.address = "localhost";
-          host.port = 7878;
-        }
-        {
-          bind.port = 8989;
-          host.address = "localhost";
-          host.port = 8989;
-        }
-        {
-          bind.port = 9091;
-          host.address = "localhost";
-          host.port = 9091;
-        }
-      ];
-    };
-  };
-
-  programs.starship.enable = true;
-  programs.starship.enableZshIntegration = true;
-
-  programs.vim.enable = true;
-
-  programs.zsh.enable = true;
-  programs.zsh.enableAutosuggestions = true;
-  programs.zsh.enableCompletion = true;
-  programs.zsh.oh-my-zsh.enable = true;
-  programs.zsh.initExtra = ''
-    . "${pkgs.nix}/etc/profile.d/nix.sh"
-  '';
-  programs.zsh.shellAliases = {
-    cat = "bat -pp";
-  };
-  programs.zsh.zplug.enable = true;
-  programs.zsh.zplug.plugins = [
-    {
-      name = "zsh-users/zsh-syntax-highlighting";
-      tags = [ "defer:2" ];
-    }
-  ];
 
   # This value determines the Home Manager release that your
   # configuration is compatible with. This helps avoid breakage
--- nix/.config/nixpkgs/linux.nix
+++ nix/.config/nixpkgs/linux.nix
@@ -1,332 +1,44 @@
 { config, lib, pkgs, ... }:
 
 {
-  home.packages = [
-    pkgs.bmon
-    pkgs.brightnessctl
-    pkgs.gnome3.gnome-calculator
-    pkgs.gnome3.gnome-screenshot
-    pkgs.grim
-    pkgs.iotop
-    pkgs.keepassxc
-    pkgs.meld
-    pkgs.networkmanager-openvpn
-    pkgs.pulsemixer
-    pkgs.python38
-    pkgs.python38Packages.i3ipc
-    pkgs.slack
-    pkgs.slurp
-    pkgs.xdg-user-dirs
-    pkgs.yarn
-  ];
+  # home.packages = [
+  #   pkgs.gnome3.gnome-calculator
+  #   pkgs.meld
+  #   pkgs.networkmanager-openvpn
+  #   pkgs.pulsemixer
+  #   pkgs.slack
+  #   pkgs.xdg-user-dirs
+  # ];
 
-  home.sessionVariables.LD_LIBRARY_PATH = "$(nixGL printenv LD_LIBRARY_PATH):$LD_LIBRARY_PATH";
-  home.sessionVariables.LOCALE_ARCHIVE = "${pkgs.glibcLocales}/lib/locale/locale-archive";
+  # home.sessionVariables.LD_LIBRARY_PATH = "$(nixGL printenv LD_LIBRARY_PATH):$LD_LIBRARY_PATH";
+  # home.sessionVariables.LOCALE_ARCHIVE = "${pkgs.glibcLocales}/lib/locale/locale-archive";
 
-  # This is only required because the gnome-keyring service doesn't seem to set it properly yet
-  home.sessionVariables.SSH_AUTH_SOCK = "/run/user/1000/keyring/ssh";
+  # # This is only required because the gnome-keyring service doesn't seem to set it properly yet
+  # # home.sessionVariables.SSH_AUTH_SOCK = "/run/user/1000/keyring/ssh";
 
-  programs.feh.enable = true;
+  # accounts.email.accounts = {
+  #   "test@example.com" = {
+  #     address = "test@example.com";
 
-  programs.firefox = {
-    enable = true;
-    package = pkgs.firefox-devedition-bin;
-  };
+  #     folders.inbox = "";
 
-  programs.ncmpcpp.enable = true;
+  #     neomutt = {
+  #       enable = true;
+  #       sendMailCommand = "/bin/true";
+  #     };
 
-  programs.rofi.enable = true;
-  programs.rofi.font = "Fira Code Retina 10";
-  programs.rofi.theme = "solarized";
-  programs.rofi.extraConfig = ''
-    rofi.show-icons: true
-  '';
+  #     passwordCommand = "/bin/true";
+  #     primary = true;
+  #     realName = "Jonjo McKay";
+  #   };
+  # };
+  # accounts.email.maildirBasePath = "tmp/maildir";
 
-  programs.vscode.enable = true;
-  programs.vscode.package = pkgs.vscodium;
-  programs.vscode.extensions = [
-    pkgs.vscode-extensions.bbenoist.Nix
-  ];
-  programs.vscode.userSettings = {
-    "debug.console.fontSize" = 12;
-    "editor.fontFamily" = "'Fira Code Retina'";
-    "editor.fontSize" = 12;
-    "editor.minimap.enabled" = false;
-    "markdown.preview.fontSize" = 12;
-    "terminal.integrated.fontSize" = 12;
-  };
+  # programs.neomutt.enable = true;
+  # programs.neomutt.sidebar.enable = true;
 
-  programs.zsh.initExtra = ''
-  if [[ -z $DISPLAY ]] && [[ $(tty) = /dev/tty1 ]]; then
-    exec nixGL sway
-  fi
-  '';
+  # services.mpd.enable = true;
+  # services.mpd.musicDirectory = "/mnt/music";
 
-  services.blueman-applet.enable = true;
-
-  services.dunst.enable = true;
-  services.dunst.settings = {
-    global = {
-      alignment = "center";
-      follow = "mouse";
-      format = "<b>%s</b>\n%b";
-      geometry = "300x50-15+49";
-      frame_width = 3;
-      frame_color = "#8EC07C";
-      horizontal_padding = 6;
-      line_height = 3;
-      padding = 6;
-      separator_color = "frame";
-      separator_height = 2;
-      transparency = 5;
-      word_wrap = "yes";
-    };
-
-    urgency_low = {
-      frame_color = "#3B7C87";
-      foreground = "#3B7C87";
-      background = "#191311";
-      timeout = 4;
-    };
-
-    urgency_normal = {
-      frame_color = "#5B8234";
-      foreground = "#5B8234";
-      background = "#191311";
-      timeout = 6;
-    };
-
-    urgency_critical = {
-     frame_color = "#B7472A";
-      foreground = "#B7472A";
-      background = "#191311";
-      timeout = 8;
-    };
-  };
-
-  services.gnome-keyring.enable = true;
-  services.gnome-keyring.components = ["pkcs11" "secrets" "ssh"];
-
-  services.gpg-agent = {
-    enable = true;
-  };
-
-  services.mpd.enable = true;
-  services.mpd.musicDirectory = "/mnt/music";
-
-  services.mpdris2.enable = true;
-
-  services.network-manager-applet = {
-    enable = true;
-  };
-  
-  services.syncthing.enable = true;
-
-  services.waybar.enable = true;
-  services.waybar.package = pkgs.waybar.override {
-    pulseSupport = true;
-  };
-  # services.waybar.config = builtins.readFile ./configs/waybar.json;
-  services.waybar.styles = builtins.readFile ./configs/waybar.css;
-
-  services.waybar.extraConfig = {
-    layer = "top";
-    
-    modules-left = ["sway/workspaces"];
-    modules-right = ["custom/waybar-media" "battery" "temperature#cpu" "temperature#gpu" "disk" "cpu" "memory" "pulseaudio" "tray" "clock"];
-    
-    clock = {
-      format = "{:%b %d %Y, %H:%M:%S}";
-      interval = 1;
-      tooltip = false;
-    };
-    
-    "custom/waybar-media" = {
-      return-type = "json";
-      exec = "${(pkgs.python38.withPackages (ps: [ps.pydbus ps.psutil])).interpreter} ${./waybar-media.py} status";
-      on-click = "${(pkgs.python38.withPackages (ps: [ps.pydbus ps.psutil])).interpreter} ${./waybar-media.py} playpause";
-      on-scroll-up = "${(pkgs.python38.withPackages (ps: [ps.pydbus ps.psutil])).interpreter} ${./waybar-media.py} previous";
-      on-scroll-down = "${(pkgs.python38.withPackages (ps: [ps.pydbus ps.psutil])).interpreter} ${./waybar-media.py} next";
-      escape = true;
-    };
-
-    "battery" = {
-      "bat" = "BAT0";
-      "interval" = 60;
-      "states" = {
-        "warning" = 30;
-        "critical" = 15;
-      };
-      "format" = "{icon} {capacity}% ({time} remaining)";
-      "format-charging" = "{icon} {capacity}% ({time} until full)";
-      "format-icons" = ["" "" "" "" ""];
-      "format-time" = "{H}:{M}";
-      "max-length" = 25;
-    };
-
-    "temperature#cpu" = {
-      interval = 2;
-      hwmon-path = "/sys/class/hwmon/hwmon2/temp1_input";
-      critical-threshold = 80;
-      format-critical = " CPU: {temperatureC}°C";
-      format = " CPU: {temperatureC}°C";
-    };
-
-    "temperature#gpu" = {
-      interval = 2;
-      hwmon-path = "/sys/class/hwmon/hwmon4/temp1_input";
-      critical-threshold = 80;
-      format-critical = " GPU: {temperatureC}°C";
-      format = " GPU: {temperatureC}°C";
-    };
-
-    "disk" = {
-      interval = 10;
-      format = " {percentage_used}%";
-      path = "/";
-    };
-
-    "cpu" = {
-        interval = 2;
-        format = " {}%";
-        max-length = 10;
-    };
-
-    "memory" = {
-        interval = 2;
-        format = " {}%";
-        max-length = 10;
-    };
-
-    "pulseaudio" = {
-        format = "{icon} {volume}%";
-        format-bluetooth = "{icon}  {volume}%";
-        format-muted = "";
-        format-icons = {
-            headphone = "";
-            hands-free = "";
-            headset = "";
-            phone = "";
-            portable = "";
-            car = "";
-            default = ["" ""];
-        };
-        scroll-step = 1;
-        on-click = "pavucontrol";
-    };
-
-    "tray" = {
-      icon-size = "21";
-      spacing = 10;
-    };
-
-    "sway/workspaces" = {
-      disable-scroll = true;
-      all-outputs = false;
-      format = "{name}:  {icon}";
-      format-icons = {
-          "1" = "";
-          "2" = "";
-          "3" = "";
-          "4" = "";
-          "5" = "";
-          "6" = "";
-          "7" = "";
-          urgent = "";
-          focused = "";
-          default = "";
-      };
-    };
-  };
-
-  wayland.windowManager.sway.enable = true;
-  wayland.windowManager.sway.config.bars = [];
-  wayland.windowManager.sway.config.colors.focused = {
-     border = "#007030";
-     background = "#007030";
-     childBorder = "#285577";
-     text = "#ffffff";
-     indicator = "2b2b2b";
-  };
-  wayland.windowManager.sway.config.colors.focusedInactive = {
-     border = "#888888";
-     background = "#2b2b2b";
-     childBorder = "#5f676a";
-     text = "#ffffff";
-     indicator = "2b2b2b";
-  };
-  wayland.windowManager.sway.config.colors.unfocused = {
-     border = "#888888";
-     background = "#2b2b2b";
-     childBorder = "#222222";
-     text = "#ffffff";
-     indicator = "2b2b2b";
-  };
-  wayland.windowManager.sway.config.colors.urgent = {
-     border = "#900000";
-     background = "#900000";
-     childBorder = "#900000";
-     text = "#ffffff";
-     indicator = "2b2b2b";
-  };
-  wayland.windowManager.sway.config.gaps.inner = 15;
-  wayland.windowManager.sway.config.gaps.top = 0;
-  wayland.windowManager.sway.config.input = {
-    "*" = {
-      xkb_layout = "gb";
-    };
-
-    "2:10:TPPS/2_Elan_TrackPoint" = {
-      pointer_accel = "0.1";
-    };
-
-    "2:7:SynPS/2_Synaptics_TouchPad" = {
-      accel_profile = "adaptive";
-      click_method = "button_areas";
-      natural_scroll = "enabled";
-      pointer_accel = "0.6";
-      tap = "enabled";
-    };
-
-    "1452:635:bcm5974" = {
-      accel_profile = "adaptive";
-      click_method = "button_areas";
-      natural_scroll = "enabled";
-      pointer_accel = "0.55";
-    };
-  };
-  wayland.windowManager.sway.config.modifier = "Mod4";
-  wayland.windowManager.sway.config.output = {
-    "*" = {
-      bg = "${./anime-minimalism-4k-4e.jpg} fill";
-    };
-  };
-  wayland.windowManager.sway.config.startup = [
-    {
-      command = "swaymsg workspace 1";
-    }
-    {
-      command = "${(pkgs.python38.withPackages (ps: [ps.i3ipc])).interpreter} ${./fader.py}";
-    }
-  ];
-  wayland.windowManager.sway.config.terminal = "nixGL alacritty";
-  wayland.windowManager.sway.config.window.border = 0;
-
-  wayland.windowManager.sway.config.keybindings = let modifier = config.wayland.windowManager.sway.config.modifier;
-  in lib.mkOptionDefault {
-    "${modifier}+0" = "workspace number 1";
-    "${modifier}+space" = "exec rofi -show drun";
-    "Print" = "exec grim -g \"$(slurp; sleep 5)\" $(xdg-user-dir PICTURES)/$(date +'%s_grim.png')";
-    "XF86AudioRaiseVolume" = "exec pactl set-sink-volume @DEFAULT_SINK@ +5%";
-    "XF86AudioLowerVolume" = "exec pactl set-sink-volume @DEFAULT_SINK@ -5%";
-    "XF86AudioMute" = "exec pactl set-sink-mute @DEFAULT_SINK@ toggle";
-    "XF86AudioMicMute" = "exec pactl set-source-mute @DEFAULT_SOURCE@ toggle";
-    "XF86MonBrightnessDown" = "exec brightnessctl set 5%-";
-    "XF86MonBrightnessUp" = "exec brightnessctl set +5%";
-    "XF86AudioPlay" = "exec playerctl play-pause";
-    "XF86AudioNext" = "exec playerctl next";
-    "XF86AudioPrev" = "exec playerctl previous";
-  };
-
-  xresources.extraConfig = builtins.readFile ./configs/xresources.conf;
+  # xresources.extraConfig = builtins.readFile ./configs/xresources.conf;
 }
